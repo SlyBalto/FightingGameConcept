1468666b1c2db94a03fb3bd5dbcfc25aa82d1348
SlyBalto <55856849+SlyBalto@users.noreply.github.com>
2019-12-17 12:40:30 -0600

    Update 12/17

---
+++ Fight Game Test/Assets/Scripts/FightManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightManager : MonoBehaviour
+{
+
+    public GameObject Player1;
+    public GameObject Player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player1.transform.position.x < Player2.transform.position.x)
+        {
+            Debug.Log("Player 1 is on the left");
+            Player1.gameObject.GetComponent<FighterStats>().facingright = true;
+            Player2.gameObject.GetComponent<FighterStats>().facingright = false;
+
+        }
+        else
+        {
+            Player1.gameObject.GetComponent<FighterStats>().facingright = false;
+            Player2.gameObject.GetComponent<FighterStats>().facingright = true;
+        }
+    }
+}
--- Fight Game Test/Assets/Scripts/FighterStats.cs
+++ Fight Game Test/Assets/Scripts/FighterStats.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FighterStats : MonoBehaviour
 {
 
-    public CharacterController2D controller;
+    public CharacterController2D Controller;
 
     public int playerID = 1;
 
@@ -13,19 +13,22 @@ public class FighterStats : MonoBehaviour
 
     public float runSpeed = 40f;
 
-    float horizontalMove = 0f;
-
-    float inputHorz = 0f;
+    float P1MoveFloat = 0f;
+    float P2MoveFloat = 0f;
 
     bool jump = false;
 
-    bool facingright = true;
+    public bool facingright = true;
 
     public GameObject attackexample;
 
     public Collider2D hurtbox;
 
-   
+    public Vector3 InstLocRight;
+
+    public Vector3 InstLocLeft;
+
+
 
 
 
@@ -38,34 +41,107 @@ public class FighterStats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        InstLocRight = ((hurtbox.transform.position) + new Vector3(1f, 0.5f, 0));
+        InstLocLeft = ((hurtbox.transform.position) + new Vector3(-1f, 0.5f, 0));
 
-        if (Input.GetButtonDown("Jump"))
+        if (playerID == 1)
         {
-            jump = true;
+            P1MoveFloat = Input.GetAxisRaw("P1Move") * runSpeed;
+        }
+        else
+            P2MoveFloat = Input.GetAxisRaw("P2Move") * runSpeed;
+        // Debug.Log("P2MoveFloat = " + P2MoveFloat);
+        //       ("WeaponNum = " + WeaponNum);
+
+        if (playerID == 1)
+        {
+            if (Input.GetButtonDown("P1Jump"))
+            {
+                jump = true;
+            }
+
+            // This is the attack
+            if (Input.GetButtonDown("P1Attack"))
+            {
+                if (facingright == true)
+                {
+                    GameObject ball;
+                    ball = Instantiate(attackexample, InstLocRight, Quaternion.identity);
+                    ball.transform.SetParent(hurtbox.transform);
+                    //print("ball.gameObject.GetComponent<AttackScript>().AttackID == " + ball.gameObject.GetComponent<AttackScript>().AttackID);
+                }
+                else
+                {
+                    GameObject ball;
+                    ball = Instantiate(attackexample, InstLocLeft, Quaternion.identity);
+                    ball.transform.SetParent(hurtbox.transform);
+                }
+            
+            }
         }
 
-        // This is the attack
-        //  if (Input.GetButtonDown("Fire1"))
-        //  {
-        //      Instantiate(attackexample, new Vector3(transform * 2.0F, 0, 0), Quaternion.identity);
-        //   }
 
-        //This is when the capsule and the AttackSphere are not
-        //the same player ID and is a successful hit
-        
+            //These are the same actions but they respond to player 2's controls if player ID = 2
+
+            if (playerID == 2)
+            {
+
+           
+
+                if (Input.GetButtonDown("P2Jump"))
+                {
+                        jump = true;
+                }
+            
+            
+
+              // This is the attack
+              if (Input.GetButtonDown("P2Attack"))
+              {
+                if (facingright == true)
+                {
+                    GameObject ball;
+                    ball = Instantiate(attackexample, InstLocRight, Quaternion.identity);
+                    ball.transform.SetParent(hurtbox.transform);
+                    ball.gameObject.GetComponent<AttackScript>().AttackID = playerID;
+                }
+                else
+                {
+                    GameObject ball;
+                    ball = Instantiate(attackexample, InstLocLeft, Quaternion.identity);
+                    ball.transform.SetParent(hurtbox.transform);
+                    ball.gameObject.GetComponent<AttackScript>().AttackID = playerID;
+                }
+              }
+            }
+
     }
-   // void OnTriggerEnter2D(Collider2D hurtbox)
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        // if (col == )
-        health = (health - 20);
-        Debug.Log("ouch!");
+   
+        void OnTriggerEnter2D (Collider2D col)
+        {
+
+        // this tests if the incoming Attack Prefab has a different ID
+        if (col.gameObject.GetComponent<AttackScript>().AttackID != playerID)
+        {
+            health = (health - 3);
+            Debug.Log("ouch!" + health);
+        }
     }
+    
+   
+
+    //This is when the capsule and the AttackSphere are not
+    //the same player ID and is a successful hit
+
+     
     void FixedUpdate()
-    {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+    { if (playerID == 1){ 
+        Controller.Move(P1MoveFloat * Time.fixedDeltaTime, false, jump);
+        jump = false;
+        }
+    else
+        Controller.Move(P2MoveFloat * Time.fixedDeltaTime, false, jump);
         jump = false;
     }
-}
 
+}
